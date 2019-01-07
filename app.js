665b4805c27221057d02de40daced5c8f5b2679f
James <43918220+jamxu88@users.noreply.github.com>
2019-01-07 11:25:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("message", (message) => {
-if (message.author.id = "531655943939686400") {
-  message.channel.send("shut up")
-}
+  if (message.author.id = "531655943939686400") {
+    message.channel.send("shut up")
+  }else {
+    return
+  }
 });
 client.login(process.env.BOT_TOKEN)

3345309da11c85656b56e03d9cb5e7a593576397
James <43918220+jamxu88@users.noreply.github.com>
2019-01-08 17:36:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 });
 client.on("message", (message) => {
   console.log(message.author.id);
-  if (message.author.id === "360965498126729220" || message.author.id === "274574060149800960") {
+  if (message.author.id === "274574060149800960") {
     message.channel.send("shut up " + message.author.toString() + " you retard");
   }
 });

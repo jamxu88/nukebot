f63416557cc7cee92402a020a115fdace4e29942
James <43918220+jamxu88@users.noreply.github.com>
2019-01-08 17:41:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 });
 client.on("message", (message) => {
   console.log(message.author.id);
-  if (message.author.id === "274574060149800960") {
+  if (message.author.id === "274574060149800960" || message.member.roles.has(527684781903839242)) {
     message.channel.send("shut up " + message.author.toString() + " you retard");
   }
 });

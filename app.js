f683b4df5f3a78d66073461873cc5c648ee46a7f
James <43918220+jamxu88@users.noreply.github.com>
2019-01-10 18:25:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ client.on("message", (message) => {
   console.log(message.author.id);
   if (message.author.id === "274574060149800960" || message.member.roles.has("527684781903839242")) {
     message.delete();
-    message.channel.send("shut up " + message.author.toString() + " you retard");
+    message.channel.send("" + message.author.toString() + ", you've been recently contributing a lot to the cancer of Meme Team. Due to the RETARDS Act of 1/10/19, you have been placed under supervision of Retard-Away™ for the time being. Thank you for your time and effort towards ridding the cancer.");
   }
 });
 client.login(process.env.BOT_TOKEN)

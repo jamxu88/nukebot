76db9d3d371697b3c575a0e360311b58d67929fb
James <43918220+jamxu88@users.noreply.github.com>
2019-01-17 21:23:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ client.on("message", (message) => {
   console.log(message.author.id);
   if(message.guild === null) {
     return;
-  }
+  }else
   if (message.member.roles.has("527684781903839242")) {
-    message.author.send("" + message.author.toString() + ", you've been recently contributing a lot to the cancer of Meme Team. Due to the RETARDS Act of 1/10/19, you have been placed under supervision of Retard-Away™ for the time being. Thank you for your time and effort towards ridding the cancer.");
+    message.author.send("" + message.author.toString() + ", you've been recently contributing a lot to the cancer of Meme Team. Due to the RETARDS Act of 1/10/19, you have been placed under supervision of Retard-Away™ for the time being. Please mind what you're saying and making sure you aren't being cancerous. Thank you for your time and effort towards ridding the cancer.");
+    message.delete();
   }
 });
 client.login(process.env.BOT_TOKEN)

a7e8240678012d7dd83e6a2bcf1a1ae007c38dab
James <43918220+jamxu88@users.noreply.github.com>
2019-02-26 16:50:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ client.on("message", (message) => {
   if (message.member.roles.has("527684781903839242")) {
     message.author.send("" + message.author.toString() + ", you've been recently contributing a lot to the cancer of Meme Team. Due to the RETARDS Act of 1/10/19, you have been placed under supervision of Retard-Away™ for the time being. Please mind what you're saying and making sure you aren't being cancerous. Thank you for your time and effort towards ridding the cancer.");
     message.delete();
+  }else
+  if (message.content.startsWith("!subgap")) {
+    message.channel.send("https://www.youtube.com/watch?v=UVxU2HzPGug")
   }
 });
 client.login(process.env.BOT_TOKEN)

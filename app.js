c47f0f054ca14d871e0c0cea00180179ced54f82
James <43918220+jamxu88@users.noreply.github.com>
2019-09-23 18:17:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ client.on("message", (message) => {
   if(message.guild === null) {
     return;
   }else
-  if (message.member.roles.has("Retard")) {
-    message.author.send("" + message.author.toString() + ", you've been recently contributing a lot to the cancer of Meme Team. Due to the RETARDS Act of 1/10/19, you have been placed under supervision of Retard-Away™ for the time being. Please mind what you're saying and making sure you aren't being cancerous. Thank you for your time and effort towards ridding the cancer.");
-    message.delete();
-  }else
   if (message.content.startsWith("!subgap")) {
     message.channel.send("https://www.youtube.com/watch?v=UVxU2HzPGug")
   }else

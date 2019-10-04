c2344f9565ad0b88506ee343833d5eae48697782
James <43918220+jamxu88@users.noreply.github.com>
2019-10-03 21:48:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.on("message", (message) => {
   }else
     
   if (message.content.startsWith("/nukelol")) {
-    if(!message.member.id(r=>["289523788822085632"].includes(r.name)) )
+    if (message.author.id !== '289523788822085632') 
       return message.reply("wow you really thought you were gonna get away with doing that didn't you.");
     message.channel.send("NUKING IN 5 SECONDS")
     setTimeout(nuke, 5000);

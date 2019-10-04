4ae2fd6f8b37ab48c27bf9d441cc8910b1433f14
James <43918220+jamxu88@users.noreply.github.com>
2019-10-03 21:33:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,48 +11,12 @@ client.on("message", (message) => {
   if(message.guild === null) {
     return;
   }else
-  if (message.content.startsWith("!subgap")) {
-    message.channel.send("https://www.youtube.com/watch?v=UVxU2HzPGug")
-  }else
-  if (message.content.toLowerCase() == "fuck") {
-    message.delete();
-    message.channel.send("Well that's a doozy");
-  }else
-  if (message.content.startsWith("@someone")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@idiot")) {
-    var member = message.author;
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@date")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@bf")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@gf")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@hocopls")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@coolkid")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("@hoco")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
-  }else
-  if (message.content.startsWith("you and me hoco?")) {
-    var member = message.guild.members.random();
-    message.channel.send(`${member}`);
+    
+  if (message.content.startsWith("/nukelol")) {
+    if(!message.member.roles.some(r=>["dad"].includes(r.name)) )
+      return message.reply("wow you really thought you were gonna get away with doing that didn't you.");
+    message.channel.send("NUKING IN 5 SECONDS")
+    setTimeout(nuke, 5000);
   }
 });
 client.login(process.env.BOT_TOKEN)

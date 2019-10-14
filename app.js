165e5c9d24ffc07d101a206dbfeb0f540ae7f29d
James <43918220+jamxu88@users.noreply.github.com>
2019-10-14 19:39:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,50 +8,51 @@ client.on("message", (message) => {
   if(message.guild === null) {
     return;
   }else
-  if(message.toLowerCase().includes("sex".toLowerCase()){
+  if(message.toLowerCase().includes("sex".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("slut".toLowerCase()){
+  if(message.toLowerCase().includes("slut".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("whore".toLowerCase()){
+  if(message.toLowerCase().includes("whore".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("slut".toLowerCase()){
+  if(message.toLowerCase().includes("slut".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("slot".toLowerCase()){
+  if(message.toLowerCase().includes("slot".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("bitch".toLowerCase()){
+  if(message.toLowerCase().includes("bitch".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("fuck".toLowerCase()){
+  if(message.toLowerCase().includes("fuck".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("stripper".toLowerCase()){
+  if(message.toLowerCase().includes("stripper".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("kinky".toLowerCase()){
+  if(message.toLowerCase().includes("kinky".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("penis".toLowerCase()){
+  if(message.toLowerCase().includes("penis".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("dick".toLowerCase()){
+  if(message.toLowerCase().includes("dick".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("vagina".toLowerCase()){
+  if(message.toLowerCase().includes("vagina".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("pussy".toLowerCase()){
+  if(message.toLowerCase().includes("pussy".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("cooch".toLowerCase()){
+  if(message.toLowerCase().includes("cooch".toLowerCase())){
     message.reply("Excuse me?")
   }else
-  if(message.toLowerCase().includes("cum".toLowerCase()){
+  if(message.toLowerCase().includes("cum".toLowerCase())){
     message.reply("Excuse me?")
   }
+  
 });
 client.login(process.env.BOT_TOKEN)
